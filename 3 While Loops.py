4b9cd3b3062e99e3299cf9451b4d8f420c355bdc
phelimkelly <95187501+phelimkelly@users.noreply.github.com>
2022-02-10 08:35:55 +0000

    Update 3 While Loops.py

--- 3 While Loops.py
+++ 3 While Loops.py
@@ -2,24 +2,22 @@
 #Hint, you'll have to create a counter variable for your start variable, and then increment it each time
 
 
-
-
 #Using a while loop to cycle through a string.....
-#Create a string variable, storing the sentence "I go to school in Ardgillan Community College"
-#Create a while loop, looping for the amount of characters in the sentence (Hint: use the len() function).
-#On each cycle of the loop print each letter from the sentence (Hint: use the string variable with square brackets [ ] at the end)
+#Create a string variable, storing the sentence "I go to school in Ardgillan
+#Community College" Create a while loop, looping for the amount of characters in
+#the sentence (Hint: use the len() function).
+#On each cycle of the loop print each letter from the sentence
+#(Hint: use the string variable with square brackets [ ] at the end)
 
 
 
-
-
-
-
-
-
-#Using a while loop to get the code "stuck" until it receives the correct info (called a Sentinel loop).
-# This loop will keep going until the user enters a number great than 100
+#Using a while loop to get the code "stuck" until it receives the correct info
+#(called a Sentinel loop). This loop will keep going until the user enters a
+#number great than 100
 #Step1: create a variable with value 0
 #Step2: create a while loop, which enters if the variable is less than 100
-#Step3: Print "Too low!", then prompt the user to enter a value, and store this input to your existing variable. 
+#Step3: Print "Too low!", then prompt the user to enter a value, and store this
+#input to your existing variable. 
 #Step4: If the loop exits, print "You now have a big enough value!"
+
+
